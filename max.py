073e2487dcb47cfbba097f1c6f1794b8832b5262
SARTHAK ANDHARE <sarthakandhare000@gmail.com>
2025-08-07 01:11:33 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,10 @@
+x=int(input("Enter first number:"))
+y=int(input("Enter first number:"))
+
+if x>y:
+    print(f"{x} is greater..!")
+elif x<y:
+    print(f"{y} is greater..!")
+else:
+    print("Both are equal..!")  
+
